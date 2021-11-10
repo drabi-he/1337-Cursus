4b7694a2090bbda9fd08dd1478c793046753eb76
Houssam-eddine Drabi <hdrabi@e1r5p7.1337.ma>
2021-11-10 14:10:15 +0100

    1337 cursus

--- LVL0/libft/ft_split.c
+++ LVL0/libft/ft_split.c
@@ -5,49 +5,52 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hdrabi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/08 11:05:30 by hdrabi            #+#    #+#             */
-/*   Updated: 2021/11/08 18:21:36 by hdrabi           ###   ########.fr       */
+/*   Created: 2021/11/10 14:09:21 by hdrabi            #+#    #+#             */
+/*   Updated: 2021/11/10 14:09:49 by hdrabi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"libft.h"
+#include "libft.h"
 
-int	is_sep(const char *str, char to_find)
-{
-	if (*str == to_find)
-		return (1);
-	return (0);
-}
-
-int	str_len(const char *str, char charset)
+int	str_len(const char *s, char c)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] && !(is_sep(str + i, charset)))
+	while (*s && (*s++ != c))
 		i++;
 	return (i);
 }
 
-int	count_world(const char *str, char charset)
+void	free_tab(char **t)
+{
+	int	i;
+
+	i = 0;
+	while (t[i])
+		free(t[i++]);
+	free(t);
+}
+
+int	count_world(const char *s, char c)
 {
 	int	i;
 	int	j;
 
 	j = 0;
-	while (*str)
+	while (*s)
 	{
-		while (*str && is_sep(str, charset))
-			str++;
-		i = str_len(str, charset);
-		str += i;
+		while (*s && *s == c)
+			s++;
+		i = str_len(s, c);
+		s += i;
 		if (i)
 			j++;
 	}
 	return (j);
 }
 
-char	*str_cpy(const char *str, int size)
+char	*str_cpy(const char *s, int size)
 {
 	char	*t;
 
@@ -56,11 +59,11 @@ char	*str_cpy(const char *str, int size)
 		return (0);
 	t[size] = 0;
 	while (size--)
-		t[size] = str[size];
+		t[size] = s[size];
 	return (t);
 }
 
-char	**ft_split(char const *s, char charset)
+char	**ft_split(char const *s, char c)
 {
 	char	**t;
 	int		size;
@@ -69,19 +72,19 @@ char	**ft_split(char const *s, char charset)
 
 	if (!s)
 		return (NULL);
-	size = count_world(s, charset);
+	size = count_world(s, c);
 	t = malloc ((size + 1) * sizeof(char *));
 	if (!t)
 		return (0);
 	i = -1;
 	while (++i < size)
 	{
-		while (*s && is_sep(s, charset))
+		while (*s && *s == c)
 			s++;
-		n = str_len(s, charset);
+		n = str_len(s, c);
 		t[i] = str_cpy(s, n);
 		if (!(t[i]))
-			return (free(t), NULL);
+			return (free_tab(t), NULL);
 		s += n;
 	}
 	t[size] = 0;
