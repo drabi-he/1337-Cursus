194c2c047ab2562ed3ad38f5caba258fdf6c3655
Houssam-eddine Drabi <hdrabi@e1r7p4.1337.ma>
2022-02-09 16:50:41 +0100

    1337 cursus

--- LVL3/minishell/minishell.c
+++ LVL3/minishell/minishell.c
@@ -6,7 +6,7 @@
 /*   By: hdrabi <hdrabi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 10:18:14 by hdrabi            #+#    #+#             */
-/*   Updated: 2022/02/08 16:25:10 by hdrabi           ###   ########.fr       */
+/*   Updated: 2022/02/09 15:55:38 by hdrabi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,10 +38,13 @@
 #define AND 9
 #define INFILE 10
 #define OUTFIL 11
+#define COMMAND 12
+#define DELIMITER 13
 #define C_FILE 'f'
 #define C_OPTION 'o'
 #define C_CMD 'c'
 
+#define COUNT 20
 /* ************************************************************************** */
 typedef struct s_tree
 {
@@ -61,6 +64,74 @@ typedef struct s_all
 
 } t_all;
 
+void ft_print_tree(t_tree *root, int space, int pos)
+{
+	if (root == NULL)
+        return;
+
+    // Increase distance between levels
+    space += COUNT;
+
+    // Process right child first
+    ft_print_tree(root->right, space, pos + 1);
+
+    // Print current node after space
+    // count
+    printf("\n");
+    for (int i = COUNT; i < space; i++)
+        printf(" ");
+    printf("p %d = t %d\n", pos, root->token);
+
+    // Process left child
+    ft_print_tree(root->left, space, pos +1);
+}
+
+void ft_tokening(t_tree *parent, t_tree *node)
+{
+	if (parent)
+	{
+		if (parent->token == 4)
+		{
+			node->type = C_FILE;
+			node->token = INFILE;
+		}
+		if (parent->token == 5)
+		{
+			node->type = C_FILE;
+			node->token = OUTFIL;
+		}
+		if (parent->token == 6)
+		{
+			node->type = C_FILE;
+			node->token = DELIMITER;
+		}
+		if (parent->token == 7)
+		{
+			node->type = C_FILE;
+			node->token = A_OUTPUT;
+		}
+	}
+}
+void ft_check_tree(t_tree *root)
+{
+	if (root == NULL)
+        return ;
+	if (root->token == 0 || root->token == 3 || root->token == 9)
+	{
+		if (!root->left || !root->right)
+			printf("4 syntax error!!\n");
+		return ;
+	}
+	if (root->token == 4 || root->token == 5 || root->token == 6 || root->token == 7)
+	{
+		if (!root->right)
+			printf("5 syntax error!!\n");
+		return ;
+	}
+	ft_tokening(root->parent, root);
+    ft_check_tree(root->right);
+    ft_check_tree(root->left);
+}
 /* ************************************************************************** */
 size_t	ft_strlen(const char *s)
 {
@@ -89,7 +160,10 @@ int	ft_strchr(char *s, int c)
 		if (s[i] == (char)c)
 		{
 			if (s[i + 1] == (char)c)
-				break ;
+			{
+				i += 2;
+				continue ;
+			}
 			return (i);
 		}
 		i++;
@@ -102,7 +176,7 @@ char	*ft_substr(char *s, unsigned int start, size_t len)
 	size_t	i;
 	char	*p;
 
-	if (!s)
+	if (!s || start < 0)
 		return (NULL);
 	if (ft_strlen(s + start) < len)
 		len = ft_strlen(s + start);
@@ -120,6 +194,38 @@ char	*ft_substr(char *s, unsigned int start, size_t len)
 	p[i] = 0;
 	return (p);
 }
+
+int ft_strstr(char *str, char *to_find, int *n)
+{
+	int	i;
+	int	j;
+	char get;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\'' || str[i] == '\"')
+		{
+			get = str[i++];
+			while (str[i] != get)
+				i++;
+		}
+		j = 0;
+		while (to_find[j])
+		{
+			if (to_find[j] == str[i])
+			{
+				if (to_find[j] == str[i + 1])
+					*n = 1;
+				return (i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (-1);
+}
+
 /* ************************************************************************** */
 
 void ft_find_error(char *str)
@@ -155,7 +261,90 @@ void ft_find_error(char *str)
 		i++;
 	}
 }
+
+void ft_find_error2(char *str)
+{
+	int i;
+	char get;
+	int cp;
+	int nb;
+
+	i = 0;
+	cp = 0;
+	while (str[i])
+	{
+		if (str[i] == '\"' || str[i] =='\'')
+		{
+			get = str[i++];
+			while (str[i] && str[i] != get)
+				i++;
+			if (!str[i])
+			{
+				printf("ERROR!!!\n");
+				break ;
+			}
+		}
+		if (ft_strchr(TABLE, str[i]) != -1)
+		{
+			get = str[i++];
+			while (str[i] == ' ')
+				i++;
+			if (ft_strchr(TABLE, str[i]) != -1)
+			{
+				if (get != str[i])
+				{
+					printf("1 syntax error!!\n");
+					return ;
+				}
+				if (get == str[i])
+				{
+					cp++;
+					if (cp == 2)
+					{
+						printf("2 syntax error!!\n");
+						return ;
+					}
+				}
+			}
+			if (get == '&' && cp == 0)
+			{
+				printf("3 syntax error!!\n");
+				return ;
+			}
+		}
+		else
+		{
+			cp = 0;
+			i++;
+		}
+	}
+}
 /* ************************************************************************** */
+
+int get_token(char c, int n)
+{
+	if (n == 0)
+	{
+		if (c == '<')
+			return (INPUT);
+		if (c == '>')
+			return (OUTPUT);
+	}
+	else
+	{
+		if (c == '<')
+			return (H_DOC);
+		if (c == '>')
+			return (A_OUTPUT);
+		if (c == '|')
+			return (OR);
+		if (c == '&')
+			return (AND);
+	}
+	return (-1);
+}
+
+
 t_tree	*ft_new_node(char t, int tok, t_tree *parent)
 {
 	t_tree *new;
@@ -170,30 +359,56 @@ t_tree	*ft_new_node(char t, int tok, t_tree *parent)
 	new->right = NULL;
 	return (new);
 }
+
 void	ft_fill_tree (char *str, t_tree **node, t_tree *parent)
 {
 	int i;
+	int j;
+	int n;
 
 	i = ft_strchr(str, '|');
+	n = 0;
 	if (i != -1)
 	{
 		node[0] = ft_new_node(C_OPTION, PIPE, parent);
-		ft_fill_tree(ft_substr(str, 0, i - 1), &node[0]->left, node[0]);
+		if (i != 0)
+			ft_fill_tree(ft_substr(str, 0, i - 1), &node[0]->left, node[0]);
 		ft_fill_tree(ft_substr(str, i + 1, ft_strlen(str)),&node[0]->right, node[0]);
 	}
-
+	else if (ft_strstr(str, TABLE, &n) != -1)
+	{
+		j = ft_strstr(str, TABLE, &n);
+		node[0] = ft_new_node(C_OPTION, get_token(str[j], n), parent);
+		if (n == 0)
+		{
+			if (j != 0)
+				ft_fill_tree(ft_substr(str, 0, j - 1), &node[0]->left, node[0]);
+			ft_fill_tree(ft_substr(str, j + 1, ft_strlen(str)),&node[0]->right, node[0]);
+		}
+		else
+		{
+			if (j != 0)
+				ft_fill_tree(ft_substr(str, 0, j - 1), &node[0]->left, node[0]);
+			ft_fill_tree(ft_substr(str, j + 2, ft_strlen(str)),&node[0]->right, node[0]);
+		}
+	}
 	else
 	{
-		node[0] = ft_new_node(C_CMD, INFILE, parent);
+		node[0] = ft_new_node(C_CMD, COMMAND, parent);
 		return ;
 	}
 }
+
 void	ft_create_tree(char *str, t_all *all)
 {
 	t_tree *root;
 
 	root = NULL;
 	ft_fill_tree(str, &root, NULL);
+
+	ft_check_tree(root);
+	ft_print_tree(root, 0, 0);
+	//printf("%d\n", root->token);
 }
 /* ************************************************************************** */
 
@@ -208,7 +423,9 @@ int main()
 	while (1)
 	{
 		str = readline("Minishell > ");
+		add_history(str);
 		ft_find_error(str);
+		ft_find_error2(str);
 		ft_create_tree(str, &all);
 	}
 	return (0);
